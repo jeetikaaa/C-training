2f48d67565450417ad429a399d858e5b85b91d99
jeetikaaa <jeetikashende@gmail.com>
2025-09-22 15:13:52 +0530

    array problems

---
+++ 1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int arr[5] ={10,20,30,40,50};
+    int b = arr[0];
+    for(int i =0; i<5;i++){
+        arr[i]=arr[i+1];
+    }
+    arr[4]=b;
+    for(int k=0;k<5;k++){
+        printf("%d",arr[k]);
+    }
+}
---
+++ 2.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+    int arr[5]={1,2,3,2,1};
+    int isTrue=1;
+    for(int i=0;i<5;i)
+
+    return 0;
+}
