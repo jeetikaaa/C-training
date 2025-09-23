3f07266a1704696cd65a51ebdf88f79f710fc17d
jeetikaaa <jeetikashende@gmail.com>
2025-09-23 13:11:01 +0530

    swap

---
+++ lect17/swap.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int swap(int *a,int *b){
+    
+    *a = *a + *b;
+    *b = *a - *b;
+    *a = *a - *b;
+    printf("%d %d", *a , *b);
+
+}
+int main(){
+    int a=10;
+    int b=20;
+    swap(&a,&b);
+    return 0;
+}
