bddb70afe1891078ab2c05c8c9155862476d56f1
jeetikaaa <jeetikashende@gmail.com>
2025-10-05 23:29:29 +0530

    larger of 2 numbers

---
+++ ifelsepractice/larof2no.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+    int a,b;
+    scanf ("%d %d , &a , &b");
+
+ if (a>b){ 
+    printf ("%d is larger\n",a);}
+ else if (b>a){
+    printf ("%d is larger\n",b);}
+ else { 
+    printf("Equal\n");}
+
+    return 0;
+}
